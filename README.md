ac3efc32295efeaffa56ee7ea5f7513719ae753e
Edward Twumasi <createdliving1000@gmail.com>
2025-01-26 02:54:10 -0800

    Chatty first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# REAL TIME
+A collection of applications for real time messaging and notifications leveraging web sockets
+
---
+++ chatty/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const app = express();
+const server = createServer(app);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(__dirname);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+server.listen(3001, () => {
+  console.log('server running at http://localhost:3001');
+});
